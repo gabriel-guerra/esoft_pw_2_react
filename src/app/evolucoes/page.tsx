dd3ebaf414ce268792c0717d32a28467d59b8b56
Gabriel S. Guerra <159430864+gabriel-guerra@users.noreply.github.com>
2024-06-21 03:08:50 +0000

    atividade 21/06

---
+++ src/app/evolucoes/page.tsx
@@ -0,0 +1,26 @@
+'use client'
+import { useSearchParams } from 'next/navigation';
+import { useState, useEffect } from 'react';
+import CardSection from "@/app/components/CardSection/CardSection";
+
+export default function Evolucoes(){
+    
+    const searchParams = useSearchParams();
+    const evolucao = searchParams.get('evolucao');
+    const [img, setImg] = useState();
+
+    useEffect(() => {
+        fetch(`https://pokeapi.co/api/v2/pokemon/${evolucao}`)
+        .then((res) => res.json())
+        .then((data) => {
+            setImg(data.sprites.front_default)
+        })
+    }, [])
+    
+    return (
+        <CardSection titulo={evolucao}>
+            <img src={img} style={{width: "300px"}}></img>
+        </CardSection>
+    )
+
+}
